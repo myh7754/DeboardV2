d8dc72accdec492627e2e23ea0283dd8c1b4df68
myh <myh4755@gmail.com>
2025-08-18 18:41:53 +0900

    first commit

---
+++ src/main/java/org/example/deboardv2/DeboardV2Application.java
@@ -0,0 +1,13 @@
+package org.example.deboardv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeboardV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeboardV2Application.class, args);
+    }
+
+}
